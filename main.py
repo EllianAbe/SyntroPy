72ec976246ce7df4deef06e6b07d62d5a5085195
ellian-abe <ellian_abe@outlook.com>
2024-04-04 17:33:08 -0300

    add some changes

---
+++ main.py
@@ -0,0 +1,4 @@
+from performance.cache import fibonacci_sequence, cachenacci_sequence
+
+fibonacci_sequence(3)
+# cachenacci_sequence(10000)
--- performance/cache.py
+++ performance/cache.py
@@ -1,30 +1,25 @@
+
+import decorators.metrics as metrics
 import functools
-import time
 
-@functools.cache
-def cachenacci(i):
-   if i == 1:
-     return 1
-   if i == 2:
-     return 1
 
-   return cachenacci(i-2) + cachenacci(i-1)
+@metrics.profile
+def cachenacci_sequence(n):
+    @functools.cache
+    def fibonacci(i):
+        return i if i < 2 else fibonacci(i - 2) + fibonacci(i - 1)
 
-def fibonacci(i):
-  if i == 1:
-    return 1
-  if i == 2:
-    return 1
-    
-  return fibonacci(i-2) + fibonacci(i-1)
+    return [fibonacci(i) for i in range(n)]
 
-init = time.time()
-for i in range(1, 35):
-   print(fibonacci(i))
-print('took ', time.time() - init)
 
-init = time.time()
-for i in range(1, 40):
-   print(cachenacci(i))
-print('took ', time.time() - init)
+@ metrics.profile
+def fibonacci_sequence(n):
+    def fibonacci(i):
+        return i if i < 2 else fibonacci(i - 2) + fibonacci(i - 1)
 
+    return [fibonacci(i) for i in range(n)]
+
+
+if __name__ == '__main__':
+    fibonacci_sequence(30)
+    cachenacci_sequence(1000)
